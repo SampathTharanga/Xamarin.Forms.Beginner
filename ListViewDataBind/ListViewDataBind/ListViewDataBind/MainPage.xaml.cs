8e81f0e429e73727127e38a8833d7cf62b77acaf
Sampath Tharanga <sampaththaranga13@gmail.com>
2018-04-20 01:24:53 +0530

    Listview, Data Binding and Item Template

---
+++ ListViewDataBind/ListViewDataBind/ListViewDataBind/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ListViewDataBind
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+
+            myList.ItemsSource = people;
+		}
+
+        public List<Data> people = new List<Data>()
+        {
+            new Data{Name="Sampath", Address="Mawanella"},
+            new Data{Name="Tharanga", Address="Anuradhapura"},
+            new Data{Name="Kumara", Address = "Mihintale"}
+        };
+	}
+}
