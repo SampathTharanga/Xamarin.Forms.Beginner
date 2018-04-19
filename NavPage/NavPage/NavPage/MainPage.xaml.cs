94155ecc0c6af29517973065d2e5c9cf75de5b83
Sampath Tharanga <sampaththaranga13@gmail.com>
2018-04-19 14:24:02 +0530

    Navigation page example working

---
+++ NavPage/NavPage/NavPage/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace NavPage
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        private void LoginEvent(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new HomePage());
+        }
+
+    }
+}
