f9523b06b782477327f74e0f40288eacb34a9d52
Sampath Tharanga <sampaththaranga13@gmail.com>
2018-04-19 22:28:00 +0530

    Simple ListView working

---
+++ SimpleListView/SimpleListView/SimpleListView/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SimpleListView
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+
+            myList.ItemsSource = new List<string>
+            {
+                "Sampath", "Tharanga", "Kumara", "Prasanna", "Ahinsha", "Kumari", "Jayalath", "Asela", "Sameera", "Sajith"
+            };
+		}
+	}
+}
