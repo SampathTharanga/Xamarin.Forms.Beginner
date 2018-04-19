6ffad290404a149d178a084f6d7f14aeaa2b2043
Sampath Tharanga <sampaththaranga13@gmail.com>
2018-04-19 15:47:16 +0530

    date and time picker working

---
+++ DateTimePicker/DateTimePicker/DateTimePicker/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DateTimePicker
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            var date = dp.Date;
+            var time = tp.Time;
+            lblDetails.Text = string.Format("Date : {0} \nTime : {1}", date, time);
+        }
+
+    }
+}
