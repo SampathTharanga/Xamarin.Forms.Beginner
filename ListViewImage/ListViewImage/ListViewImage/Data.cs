c7ded1f46a62f5b9c9093713a4276f5a07fba3e8
Sampath Tharanga <sampaththaranga13@gmail.com>
2018-04-20 12:19:44 +0530

    Text Cell and Image Cell in Listview

---
+++ ListViewImage/ListViewImage/ListViewImage/Data.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListViewImage
+{
+    public class Data
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string ContactPhoto { get; set; }
+    }
+}
---
+++ ListViewImage/ListViewImage/ListViewImage/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ListViewImage
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+
+            myList.ItemsSource = people;
+        }
+
+        public List<Data> people = new List<Data>()
+        {
+                new Data{ Name = "Sampath Tharanga", Address = "C-73, Dehimaduwa, Mawanella", ContactPhoto = "image1.jpg"},
+                new Data{ Name = "Asela Ranathunga", Address = "Rajapihilla Road, Kurunegala", ContactPhoto = "image2.jpg"},
+                new Data{ Name = "Kura Pathirana", Address = "Panala Road, Panadura", ContactPhoto = "image3.jpg"},
+                new Data{ Name = "Nirosha Chandrasena", Address = "Police Junction, Thambuttegama", ContactPhoto = "image4.jpg"},
+                new Data{ Name = "Jayalath Perera", Address = "Payagala, Kalutara", ContactPhoto = "image5.jpg"}
+        };
+    }
+}
