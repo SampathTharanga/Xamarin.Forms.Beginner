bf91430907cd31b5f40efc8007509743d738890e
Sampath Tharanga <sampaththaranga13@gmail.com>
2018-04-20 14:45:15 +0530

    ViewCell in ListView

---
+++ ListViewViewCell/ListViewViewCell/ListViewViewCell/Data.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListViewViewCell
+{
+    public class Data
+    {
+        public string Name { set; get; }
+        public string Address { get; set; }
+        public int Id { get; set; }
+    }
+}
---
+++ ListViewViewCell/ListViewViewCell/ListViewViewCell/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ListViewViewCell
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+
+            myList.ItemsSource = people;
+		}
+
+        public List<Data> people = new List<Data>()
+        {
+            new Data { Name = "Sampath Tharanga", Address = "Dehimaduwa, Mawanella", Id = 1},
+            new Data { Name = "Kumara Sandaruwan", Address = "Dehiovita, Avissawella", Id = 2},
+            new Data { Name = "Roshan Kumara", Address = "Dikwella, Matara", Id = 3 },
+            new Data { Name = "Asela Ranathunga", Address = "Hadapangala, Horana", Id = 4 }
+        };
+	}
+}
