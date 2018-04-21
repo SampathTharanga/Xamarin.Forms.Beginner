173b20a563969eaee9a9da44e80088a7c2641199
Sampath Tharanga <sampaththaranga13@gmail.com>
2018-04-21 22:18:57 +0530

    Check internet connectivity

---
+++ WebViewInternet/WebViewInternet/WebViewInternet/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Plugin.Connectivity;
+
+namespace WebViewInternet
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+            CheckConnectivity();
+
+        }
+
+
+
+        protected void CheckConnectivity()
+        {
+            if (!CrossConnectivity.Current.IsConnected)
+                lblCheck.Text = "Check for your connection!";
+            else
+                lblCheck.Text = "Connected!.";
+        }
+    }
+}
