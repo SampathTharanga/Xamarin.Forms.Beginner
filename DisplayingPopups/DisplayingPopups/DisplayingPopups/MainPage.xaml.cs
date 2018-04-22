b7099beae7100aed03fb75f24a240ea370fb9f3c
Sampath Tharanga <sampaththaranga13@gmail.com>
2018-04-22 15:22:16 +0530

    Displaying Popup message

---
+++ DisplayingPopups/DisplayingPopups/DisplayingPopups/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DisplayingPopups
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        private async void btnClicked(object sender, EventArgs e)
+        {
+            var response = await DisplayAlert("Messsage", "Do you wanna change color(SeaGreen)", "YES", "NO");
+            if (response)
+                this.BackgroundColor = Color.SeaGreen;
+            else
+                this.BackgroundColor = Color.White;
+        }
+
+    }
+}
