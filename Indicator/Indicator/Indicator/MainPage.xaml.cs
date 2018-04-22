6063b06616e0dc63e25185871084b839f2da00ae
Sampath Tharanga <sampaththaranga13@gmail.com>
2018-04-22 11:37:07 +0530

    Working with Activity Indicator

---
+++ Indicator/Indicator/Indicator/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using Plugin.Connectivity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Indicator
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        public void GetData(object sender, EventArgs e)
+        {
+            CheckConnectivity();
+            var url = "http://" + EnryUrl.Text;
+            Browser.Source = url;
+        }
+
+        private void CheckConnectivity()
+        {
+            if (CrossConnectivity.Current.IsConnected)
+            {
+                //DisplayAlert("Message", "Internet connected!", "OK");
+            }
+            else
+            {
+                DisplayAlert("Message", "No internet connection!", "OK");
+            }
+        }
+
+        private void Browser_Navigated(object sender, WebNavigatedEventArgs e)
+        {
+            progress.IsVisible = false;
+            progress.IsRunning = false;
+        }
+
+        private void Browser_Navigating(object sender, WebNavigatingEventArgs e)
+        {
+            progress.IsVisible = true;
+            progress.IsRunning = true;
+        }
+
+    }
+}
