31495128ac89a0a0263c29c652d4899edfcf7e9a
Sampath Tharanga <sampaththaranga13@gmail.com>
2018-05-01 23:25:54 +0530

    working with switch

---
+++ SwitchExample/SwitchExample/SwitchExample/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SwitchExample
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        private void switch2_Toggled(object sender, ToggledEventArgs e)
+        {
+            bool value = e.Value;
+            sv2.Text = value.ToString();
+        }   
+    }
+}
