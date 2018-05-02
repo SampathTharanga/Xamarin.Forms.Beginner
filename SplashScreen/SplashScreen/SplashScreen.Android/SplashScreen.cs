bb5b8f2342e8c322a820d1323e7a2a01e7a4745b
Sampath Tharanga <sampaththaranga13@gmail.com>
2018-05-02 12:35:38 +0530

    Working with Splash Screen

---
+++ SplashScreen/SplashScreen/SplashScreen.Android/SplashScreen.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace SplashScreen.Droid
+{
+    [Activity(Label = "SplashScreen", Icon = "@drawable/icon", Theme = "@style/splashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+
+    public class SplashScreen : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            System.Threading.Thread.Sleep(500); // Let's wait awhile.....
+            StartActivity(typeof(MainActivity));
+        }
+    }
+}
