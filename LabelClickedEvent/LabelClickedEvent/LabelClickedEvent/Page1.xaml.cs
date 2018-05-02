cc231e93df8a206756ce11a6e3e3588365d0c50c
Sampath Tharanga <sampaththaranga13@gmail.com>
2018-05-02 21:22:41 +0530

    Working with Label Clicked Event

---
+++ LabelClickedEvent/LabelClickedEvent/LabelClickedEvent/Page1.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LabelClickedEvent
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Page1 : ContentPage
+	{
+		public Page1 ()
+		{
+			InitializeComponent ();
+		}
+
+        private async void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Page2());
+        }
+
+    }
+}
