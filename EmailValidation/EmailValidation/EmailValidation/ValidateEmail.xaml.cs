083f546661781ea76fe41d18703e9277c4ef150f
Sampath Tharanga <sampaththaranga13@gmail.com>
2018-05-03 00:52:20 +0530

    Working with Email validation

---
+++ EmailValidation/EmailValidation/EmailValidation/ValidateEmail.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace EmailValidation
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ValidateEmail : ContentPage
+	{
+		public ValidateEmail ()
+		{
+			InitializeComponent ();
+		}
+
+        public void Button_Clicked(object sender, EventArgs e)
+        {
+            var email = EnterEmail.Text;
+            var emailPttern = "^([\\w\\.\\-]+)@([\\w\\-]+)((\\.(\\w){2,3})+)$";
+
+            if (!string.IsNullOrWhiteSpace(email) && (Regex.IsMatch(email, emailPttern)))
+                LabelError.Text = "Email verified!";
+            else
+                LabelError.Text = "Email verification faild!";
+        }
+
+    }
+}
