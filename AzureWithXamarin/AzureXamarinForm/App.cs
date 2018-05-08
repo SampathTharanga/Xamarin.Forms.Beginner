4498cb72b90d3e56e5a3456cb1929b940b1c234b
Sampath Tharanga <sampaththaranga13@gmail.com>
2018-05-08 11:10:04 +0530

    working with Azure Cloud

--- AzureWithXamarin/AzureXamarinForm/App.cs
+++ AzureWithXamarin/AzureXamarinForm/App.cs
@@ -6,15 +6,7 @@ namespace AzureXamarinForm
 {
 	public class App : Application
 	{
-        /*
-        REFERENCES:
-        https://docs.microsoft.com/en-gb/azure/app-service-mobile/app-service-mobile-xamarin-forms-get-started
-        https://www.c-sharpcorner.com/article/create-a-xamarin-forms-app-using-azure-mobile-services/
-        https://blog.xamarin.com/getting-started-azure-mobile-apps-easy-tables/
-        */
-
-
-        public App ()
+		public App ()
 		{
 			// The root page of your application
 			MainPage = new TodoList();
