a084661c68b5a33d8d0a881f574a60f917ebec56
Sampath Tharanga <sampaththaranga13@gmail.com>
2018-05-08 11:08:35 +0530

    Working withAzure cloud

---
+++ AzureWithXamarin/AzureXamarinForm/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace AzureXamarinForm
+{
+	public static class Constants
+	{
+		// Replace strings with your Azure Mobile App endpoint.
+		public static string ApplicationURL = @"https://azurexamarinform.azurewebsites.net";
+	}
+}
+
